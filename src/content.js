41382ed95471c75c6e97fbe69b1ea14b077d53dd
N. Escobar <nick@nickesc.io>
2025-09-26 01:02:44 -0400

    add ai mode button removal and options

--- src/content.js
+++ src/content.js
@@ -1,48 +1,69 @@
+const browserApi = chrome ? chrome : browser;
+
 const patterns = [
-  /übersicht mit ki/i, // de
-  /ai overview/i, // en
-  /prezentare generală generată de ai/i, // ro
-  /AI による概要/, // ja
-  /Обзор от ИИ/, // ru
-  /AI 摘要/, // zh-TW
-  /AI-overzicht/i, // nl
-  /Vista creada con IA/i, // es
-  /Přehled od AI/i, // cz
+    /übersicht mit ki/i, // de
+    /ai overview/i, // en
+    /prezentare generală generată de ai/i, // ro
+    /AI による概要/, // ja
+    /Обзор от ИИ/, // ru
+    /AI 摘要/, // zh-TW
+    /AI-overzicht/i, // nl
+    /Vista creada con IA/i, // es
+    /Přehled od AI/i, // cz
 ];
 
-const observer = new MutationObserver(() => {
-  // each time there's a mutation in the document see if there's an ai overview to hide
-  const mainBody = document.querySelector('div#rcnt');
-  const aiText = [...mainBody?.querySelectorAll('h1, h2')].find(e => patterns.some(pattern => pattern.test(e.innerText)));
+function removeAISections() {
+    browserApi.storage.sync.get(["enabled", "overview"],  ({ enabled, overview }) => {
+        if (enabled === true || enabled === undefined) {
+            const searchButtons = document.querySelectorAll('.olrp5b, .plR5qb');
+            searchButtons.forEach(element => element.style.display = "none");
+        }
 
-  var aiOverview = aiText?.closest('div#rso > div'); // AI overview as a search result
-  if (!aiOverview) aiOverview = aiText?.closest('div#rcnt > div'); // AI overview above search results
+        if (overview === true || overview === undefined) {
+            // each time there's a mutation in the document see if there's an ai overview to hide
+            const mainBody = document.querySelector('div#rcnt');
+            const aiText = [...mainBody?.querySelectorAll('h1, h2')].find(e => patterns.some(pattern => pattern.test(e.innerText)));
 
-  // Hide AI overview
-  if (aiOverview) aiOverview.style.display = "none";
+            var aiOverview = aiText?.closest('div#rso > div'); // AI overview as a search result
+            if (!aiOverview) aiOverview = aiText?.closest('div#rcnt > div'); // AI overview above search results
 
-  // Restore padding after header tabs
-  const headerTabs = document.querySelector('div#hdtb-sc > div');
-  if (headerTabs) headerTabs.style.paddingBottom = "12px";
+            // Hide AI overview
+            if (aiOverview) aiOverview.style.display = "none";
 
-  // For debugging
-  // console.log([...mainBody?.querySelectorAll('h1, h2')].map(e => { return { text: e.innerText, obj: e }}));
-  const mainElement = document.querySelector('[role="main"]');
-  if (mainElement) {
-    mainElement.style.marginTop = "24px";
-  }
+            // Restore padding after header tabs
+            const headerTabs = document.querySelector('div#hdtb-sc > div');
+            if (headerTabs) headerTabs.style.paddingBottom = "12px";
 
-  // Remove entries in "People also ask" section if it contains "AI overview"
-  const peopleAlsoAskAiOverviews = [
-    ...document.querySelectorAll("div.related-question-pair"),
-  ].filter((el) => patterns.some((pattern) => pattern.test(el.innerHTML)));
+            // For debugging
+            // console.log([...mainBody?.querySelectorAll('h1, h2')].map(e => { return { text: e.innerText, obj: e }}));
+            const mainElement = document.querySelector('[role="main"]');
+            if (mainElement) {
+                mainElement.style.marginTop = "24px";
+            }
 
-  peopleAlsoAskAiOverviews.forEach((el) => {
-    el.parentElement.parentElement.style.display = "none";
+            // Remove entries in "People also ask" section if it contains "AI overview"
+            const peopleAlsoAskAiOverviews = [
+                ...document.querySelectorAll("div.related-question-pair"),
+            ].filter((el) => patterns.some((pattern) => pattern.test(el.innerHTML)));
+
+            peopleAlsoAskAiOverviews.forEach((el) => {
+                el.parentElement.parentElement.style.display = "none";
+            });
+        }
+    });
+}
+
+browserApi.storage.onChanged.addListener((changes, namespace) => {
+    removeAISections();
   });
+
+const observer = new MutationObserver(() => {
+    removeAISections();
 });
 
 observer.observe(document, {
-  childList: true,
-  subtree: true,
+    childList: true,
+    subtree: true,
 });
+
+removeAISections();
